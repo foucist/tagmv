24d54055475df080ea6b71a7be318917587ec69a
James Robey <james.robey@gmail.com>
2016-05-14 17:08:51 -0400

    Initial commit

---
+++ lib/yo_tag.rb
@@ -0,0 +1,5 @@
+require "yo_tag/version"
+
+module YoTag
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'yo_tag'
+
+require 'minitest/autorun'
