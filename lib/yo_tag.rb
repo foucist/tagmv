3b492da7f05e6e536d6968dd57173d7d1a216d14
James Robey <james.robey@gmail.com>
2016-05-14 19:23:55 -0400

    Move file into tag dirs & tests

--- lib/yo_tag.rb
+++ lib/yo_tag.rb
@@ -1,5 +1,31 @@
 require "yo_tag/version"
+require 'fileutils'
 
 module YoTag
-  # Your code goes here...
+
+  class TagFS
+    @root = File.expand_path('~/t')
+    class << self
+      attr_accessor :root
+    end
+
+    attr_accessor :tags, :files
+    def initialize(tags, files = nil)
+      @tags = tags
+      @files = files
+    end
+
+    def target_dir
+      File.join(TagFS.root, *tags)
+    end
+
+    def prep_dirs
+      FileUtils.mkdir_p(target_dir) 
+    end
+
+    def move_file
+      prep_dirs && FileUtils.mv(files, target_dir)
+    end
+  end
+
 end
--- test/yo_tag_test.rb
+++ test/yo_tag_test.rb
@@ -1,11 +1,52 @@
 require 'test_helper'
 
 class YoTagTest < Minitest::Test
+  def before 
+    @dir = Dir.mktmpdir
+    YoTag::TagFS.root = @dir
+  end
+
+  def after
+    FileUtils.remove_entry @dir
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::YoTag::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_uses_tmp_dir
+    dir = '/tmp/foobar'
+    YoTag::TagFS.root = dir
+    tfs = YoTag::TagFS.new('a')
+    assert tfs.target_dir == dir + '/a'
+  end
+
+  def test_it_makes_tag_dirs
+    before
+
+    tags = ['a', 'b', 'c']
+    tfs = YoTag::TagFS.new(tags)
+
+    before = File.exist?(tfs.target_dir)
+    assert_equal false, before
+
+    tfs.prep_dirs
+    assert File.exist?(tfs.target_dir)
+
+    after
+  end
+
+  def test_it_moves_file_to_tag_dirs
+    before
+    tags = ['a', 'b', 'c']
+
+    file = File.join(@dir, 'timestamp')
+    assert FileUtils.touch(file)
+
+    tfs = YoTag::TagFS.new(tags, file)
+    tfs.move_file
+    assert File.exist?(File.join(tfs.target_dir, 'timestamp'))
+
+    after
   end
 end
